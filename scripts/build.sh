fb322a0e19d3f8d735a568b469feda8948c185ff
Atharva Naphade <naphade21@gmail.com>
2024-08-09 19:27:35 +0530

    Delete old project.
    Signed-off-by: Atharva Naphade <naphade21@gmail.com>

--- scripts/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Build bootloader and kernel
-make -C src
--- scripts/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Run QEMU
-qemu-system-aarch64 -M virt -cpu cortex-a72 -m 1024 -kernel src/build/kernel.elf
